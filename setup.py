9633a3f16ebcc9e962a44caacb92e407017c092e
brando90 <brandojazz@gmail.com>
2019-01-28 21:23:18 -0600

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='my_tf_proj', #project name
+    version='0.1.0', # TODO
+    description='my depedencies for gamepad',
+    #url
+    author='Brando Miranda',
+    author_email='miranda9@illinois.edu',
+    license='MIT',
+    packages=[], # TODO
+    install_requires=['networkx','matplotlib']
+    # matplotlib might give problems
+)
